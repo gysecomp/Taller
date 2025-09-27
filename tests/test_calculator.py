52116b1a0aea5870f491cd6ac14d27a5f76dedb3
gysecomp <gysecomp@gmail.com>
2025-09-27 05:14:14 -0500

    Prueba de error

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,7 +4,7 @@ from src.calculator import suma, resta
 class TestCalculator(unittest.TestCase):
 
     def test_suma(self):
-        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(2, 3), 6)
 
     def test_resta(self):
         self.assertEqual(resta(5, 2), 3)
